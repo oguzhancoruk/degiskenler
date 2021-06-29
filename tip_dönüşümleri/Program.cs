485206147052b870a9cc2a587ca0a31890e81d63
oguzhan coruk <coruk_61@hotmail.com>
2021-06-29 19:26:04 +0300

    Add files via upload

---
+++ tip_dönüşümleri/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace tip_dönüşümleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Implicit Conversion (bilinçsiz dönünüşüm)
+           byte a=5;
+           sbyte b=30;
+           short c=10;
+
+           int d=a+b+c;
+           Console.WriteLine("d:"+d);
+
+           long h=d;
+           Console.WriteLine("d:"+h);
+
+           float i=h;
+           Console.WriteLine("d:"+i);
+
+           string e="oğuzhan";
+           char f='k';
+           object g=e+f+d;
+           Console.WriteLine("d:"+g);
+
+
+
+
+           // Exclicit Conversion (bilinçli dönüşüm)
+
+           int x=4;
+           byte y=(byte)x;
+           Console.WriteLine("y:"+y);
+           int z=100;
+           byte t=(byte)z;
+           Console.WriteLine("t:"+t);
+
+           float w=18.3f;
+           byte v=(byte)w;
+           Console.WriteLine("v:"+v);
+           
+           //ToString Metodu
+
+           int xx =6;
+           string yy=xx.ToString();Console.WriteLine("yy."+yy);
+
+           string zz=12.5f.ToString();
+           Console.WriteLine("zz:"+zz);
+
+           //System.Convert
+
+           string s1="10",s2="20";
+           int sayi1,sayi2;
+           int toplam;
+
+           sayi1=Convert.ToInt32(s1);
+           sayi2=Convert.ToInt32(s2);
+
+           toplam=sayi1+sayi2;
+           Console.WriteLine("Toplam:"+toplam);
+        //PARSE
+
+        ParseMethod();
+        }
+
+        public static void ParseMethod()
+        {
+            string metin1="10";
+            string metin2="10,25";
+            int rakam1;
+            double double1;
+            rakam1=Int32.Parse(metin1);
+            double1=Double.Parse(metin2);
+
+            Console.WriteLine("rakam1:"+rakam1);
+            Console.WriteLine("double:"+double1);
+        }
+
+        
+    }
+}
