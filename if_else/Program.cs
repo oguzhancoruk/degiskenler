191e3f955e98309841ffb1d94560d8e4bb3e058b
oguzhan coruk <coruk_61@hotmail.com>
2021-06-30 17:31:41 +0300

    Add files via upload

---
+++ if_else/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time=DateTime.Now.Hour;
+
+            if(time>=6 && time<=11)
+            Console.WriteLine( "günaydın");
+            else if(time <18)
+            Console.WriteLine("İyi günler");
+            else
+            Console.WriteLine("iyigeceler");
+
+            string sonuc= time<=18 ? "iyi günler":"iyigeceler";
+            sonuc= time>=6 && time<11 ?"günaydın":time<=18 ? "iyi günler":"iyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
