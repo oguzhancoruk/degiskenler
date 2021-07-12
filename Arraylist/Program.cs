16089f6c4b7214f665d75f8828d1ca38932dd9d9
oguzhan coruk <coruk_61@hotmail.com>
2021-07-12 17:20:58 +0300

    Add files via upload

---
+++ Arraylist/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste=new ArrayList(); 
+           // liste.Add("oğuzhan");
+            //liste.Add(2);
+            //liste.Add(true);
+            //liste.Add('a');
+
+            // verilere erişme
+
+            //Console.WriteLine(liste[1]);
+            //foreach (var item in liste)
+
+            //{
+              //  Console.WriteLine(item);
+
+                
+           // }
+
+            //addrange
+            Console.WriteLine("****************");
+
+           // string[] renkler={"kırmızı","sarı","yeşil"};
+            List<int>sayılar=new List<int>(){1,8,3,7,9,92,15};
+           // liste.AddRange(renkler);
+            liste.AddRange(sayılar);
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            //sort 
+            Console.WriteLine("****sort***");
+            liste.Sort();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+
+            }
+
+            //binary search
+
+            Console.WriteLine("*************");
+            Console.WriteLine(liste.BinarySearch(9));
+
+            //reverse
+            Console.WriteLine("**********");
+            liste.Reverse();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("********");
+            liste.Clear();
+
+            foreach (var item in liste)
+
+            {
+            Console.WriteLine(item);    
+            }
+
+        }
+    }
+}
